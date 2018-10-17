6046529ba8934608e838218443daa798c90ab044
Nathan Friedly <nathan.friedly@tanium.com>
2018-10-17 13:32:15 -0400

    css media query

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,12 @@ const Layout = ({children}) => {
     <div className="layout">
       <style jsx>{`
         a { color: teal; }
-        .layout { max-width: 400px; margin: auto; }
+
+        .layout { width: 100%; margin: 5px; }
+
+        @media(min-width: 600px) {
+          .layout { max-width: 400px; margin: auto; }
+        }
       `}</style>
 
       <Link href="/"><a>Home</a></Link>
