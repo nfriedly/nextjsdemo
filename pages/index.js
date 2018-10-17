38ed7e032c4dc25ff9b91e62434e8a5c7924050a
Nathan Friedly <nathan.friedly@tanium.com>
2018-10-17 13:46:03 -0400

    intro text

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,16 @@
 export default function() {
-  return (<h1>Hello world!</h1>);
+  return (
+    <div>
+
+      <h1>Next.js: React framework with server-side-rendering</h1>
+      <h2><small>by</small> Nathan Friedly</h2>
+
+      <ul>
+        <li><a href="https://nextjs.org/">nextjs.org</a></li>
+        <li><a href="https://github.com/nfriedly/nextjsdemo/">
+          github.com/nfriedly/nextjsdemo</a></li>
+      </ul>
+
+    </div>
+  );
 }
