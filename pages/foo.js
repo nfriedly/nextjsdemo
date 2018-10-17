72c803d54f8f96c54ff46f84f68109b189f959d5
Nathan Friedly <nathan.friedly@tanium.com>
2018-10-17 13:25:10 -0400

    getInitialProps

--- pages/foo.js
+++ pages/foo.js
@@ -1,10 +1,19 @@
 import Layout from '../components/layout'
 
-export default function() {
+const FooPage = function(props) {
   return (
     <Layout>
-      <p>Foo!</p>
+
+      <h1>Hello from {props.renderedAt}</h1>
+
     </Layout>
   );
 }
 
+FooPage.getInitialProps = function(ctx) {
+  return {
+    renderedAt: ctx.req ?  'server' : 'client'
+  }
+}
+
+export default FooPage
