c3ac9618c63668db232f6f6ed815497b857f6622
Nathan Friedly <nathan.friedly@tanium.com>
2018-10-17 13:26:27 -0400

    local style

--- components/layout.js
+++ components/layout.js
@@ -2,10 +2,16 @@ import Link from 'next/link'
 
 const Layout = ({children}) => {
   return (
-    <div>
+    <div className="layout">
+      <style jsx>{`
+        a { color: teal; }
+        .layout { max-width: 400px; margin: auto; }
+      `}</style>
+
       <Link href="/"><a>Home</a></Link>
       {' '}
       <Link href="/foo"><a>Foo</a></Link>
+
       {children}
     </div>
   );
